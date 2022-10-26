91cc52bedfd31f2a1799015a524f3617dd1b0c93
Buğra Haskan <bugrahaskan@outlook.com>
2022-10-26 21:24:03 +0300

    last update before sql queries

--- views.py
+++ views.py
@@ -24,39 +24,52 @@ def views():
     label5 = tk.Label(frame1, text="Region Name:")
     entry5 = tk.Entry(frame1, textvariable=data.var5)
     
+    def search():
+        for child in table.get_children():
+            if table.item(child)["values"][1] == data.var1.get():
+                table.focus(child)
+                table.see(child)
+                table.selection_set(child)
+                print(table.index(child)+1)
+                print(data.var1.get() in table.item(child)["values"])
+                break
+            
+        
 
     button1 = tk.Button(frame1, text="Add", width=5, command=data.insert_data)
     button2 = tk.Button(frame1, text="Update", width=5, command=data.update_data)
     button3 = tk.Button(frame1, text="Delete", width=5, command=data.del_data)
-    button4 = tk.Button(frame1, text="Search", width=5, command=data.search_data)
+    button4 = tk.Button(frame1, text="Search", width=5, command=search)
 
-    cols = ('Country Name', 'Country Code', 'Continent', 'Airport Name', 'Region Name')
+    cols = ('ID', 'Country Name', 'Code', 'Continent', 'Airport Name', 'Region Name')
     table = ttk.Treeview(window, columns=cols, show='headings')
-    table.column("Country Code", width=100)
+    table.column("ID", width=50)
+    table.column("Code", width=50)
     table.column("Continent", width=100)
     for col in cols:
         table.heading(col, text=col)
 
     def selectItem(a):
         curItem = table.focus()
-        data.var1.set(table.item(curItem)["values"][0])
-        data.var2.set(table.item(curItem)["values"][1])
-        data.var3.set(table.item(curItem)["values"][2])
-        data.var4.set(table.item(curItem)["values"][3])
-        data.var5.set(table.item(curItem)["values"][4])
+        data.var1.set(table.item(curItem)["values"][1])
+        data.var2.set(table.item(curItem)["values"][2])
+        data.var3.set(table.item(curItem)["values"][3])
+        data.var4.set(table.item(curItem)["values"][4])
+        data.var5.set(table.item(curItem)["values"][5])
     table.bind('<Double-Button-1>', selectItem)
 
     rows = data.get_data()
     for i, (country, code, continent, airport, region) in enumerate(rows, start=1):
-        table.insert("", "end", values=(country, code, continent, airport, region))
+        table.insert("", "end", values=(i, country, code, continent, airport, region))
+
 
     label1.place(x=40, y=20), entry1.place(x=150, y=20)
     label2.place(x=40, y=60), entry2.place(x=150, y=60)
     label3.place(x=40, y=100), entry3.place(x=150, y=100)
     label4.place(x=40, y=140), entry4.place(x=150, y=140)
     label5.place(x=40, y=180), entry5.place(x=150, y=180)
-    button1.place(x=400, y=20), button2.place(x=400, y=60)
-    button3.place(x=400, y=100), button4.place(x=400, y=140)
+    button1.place(x=400, y=40), button2.place(x=400, y=80)
+    button3.place(x=400, y=120), button4.place(x=400, y=160)
     frame1.pack()
     table.pack()
     window.mainloop()
