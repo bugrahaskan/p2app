4a4d63953c3748f6a356363c3e83ba2b3f346107
Buğra Haskan <bugrahaskan@outlook.com>
2022-10-26 17:00:43 +0300

    update

--- engine.py
+++ engine.py
@@ -13,25 +13,87 @@ class Data(tk.Tk):
         self.var3 = tk.StringVar()
         self.var4 = tk.StringVar()
 
+    # @staticmethod
+    # def connection_decorator(func):
+    #     def connection(self):
+    #         self.database = sqlite3.connect(self.path)
+    #         func()
+    #         self.database.close()
+    #     return connection
+
+    # @connection_decorator
     def get_data(self):
+        self.database = sqlite3.connect(self.path)
         _db = self.database.cursor()
         _db.execute("SELECT country.name, country.country_code, region.name, \
                     region.keywords FROM country INNER JOIN region \
                     ON country.country_id = region.country_id \
                     ORDER BY country.name")
         rows = _db.fetchall()
-        #_db.close()
         self.database.close()
 
         return rows
 
     def del_data(self):
-        pass
-
+        self.database = sqlite3.connect(self.path)
+        message='Following Record Deleted Succesfully: \
+                {}, {}, {}'.format(self.var1.get(),
+                                    self.var2.get(),
+                                    self.var3.get())
+        try:
+            # SQL Query
+            messagebox.showinfo("title", message)
+        except Exception as e:
+            print(e)
+        finally:
+            self.var1.set("")
+            self.var2.set("")
+            self.var3.set("")
+            self.var4.set("")
+            self.database.close()
+        
     def update_data(self):
-        pass
+        self.database = sqlite3.connect(self.path)
+        message='Following Record Updated Succesfully: \
+                {}, {}, {}'.format(self.var1.get(),
+                                    self.var2.get(),
+                                    self.var3.get())
+        try:
+            # SQL Query
+            messagebox.showinfo("title", message)
+        except Exception as e:
+            print(e)
+        finally:
+            self.var1.set("")
+            self.var2.set("")
+            self.var3.set("")
+            self.var4.set("")
+            self.database.close()
 
+    def insert_data(self):
+        self.database = sqlite3.connect(self.path)
+        message='Following Record Added Succesfully: \
+                {}, {}, {}'.format(self.var1.get(),
+                                    self.var2.get(),
+                                    self.var3.get())
+        try:
+            # SQL Query
+            messagebox.showinfo("title", message)
+        except Exception as e:
+            print(e)
+        finally:
+            self.var1.set("")
+            self.var2.set("")
+            self.var3.set("")
+            self.var4.set("")
+            self.database.close()
 
-# if __name__ == "__main__":
-#     data = Data()
-#     data.get_data()
+    def search_data(self):
+        self.database = sqlite3.connect(self.path)
+        try:
+            # SQL Query
+            messagebox.showinfo("title", "Search Query")
+        except Exception as e:
+            print(e)
+        finally:
+            self.database.close()
--- views.py
+++ views.py
@@ -22,9 +22,10 @@ def views():
     label4 = tk.Label(frame1, text="Region Keyword:")
     entry4 = tk.Entry(frame1, textvariable=data.var4)
 
-    button1 = tk.Button(frame1, text="Add", width=5, command=window.destroy)
-    button2 = tk.Button(frame1, text="Update", width=5, command=window.destroy)
-    button3 = tk.Button(frame1, text="Delete", width=5, command=window.destroy)
+    button1 = tk.Button(frame1, text="Add", width=5, command=data.insert_data)
+    button2 = tk.Button(frame1, text="Update", width=5, command=data.update_data)
+    button3 = tk.Button(frame1, text="Delete", width=5, command=data.del_data)
+    button4 = tk.Button(frame1, text="Search", width=5, command=data.search_data)
 
     cols = ('Country Name', 'Country Code', 'Region Name', 'Region Keyword')
     table = ttk.Treeview(window, columns=cols, show='headings')
@@ -43,13 +44,12 @@ def views():
     for i, (country, code, region, keyword) in enumerate(rows, start=1):
         table.insert("", "end", values=(country, code, region, keyword))
     
-    
-
     label1.place(x=40, y=20), entry1.place(x=150, y=20)
     label2.place(x=40, y=60), entry2.place(x=150, y=60)
     label3.place(x=40, y=100), entry3.place(x=150, y=100)
     label4.place(x=40, y=140), entry4.place(x=150, y=140)
-    button1.place(x=400, y=40), button2.place(x=400, y=80), button3.place(x=400, y=120)
+    button1.place(x=400, y=20), button2.place(x=400, y=60)
+    button3.place(x=400, y=100), button4.place(x=400, y=140)
     frame1.pack()
     table.pack()
     window.mainloop()
